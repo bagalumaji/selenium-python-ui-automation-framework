b80d100a0dbd58d7ecf7992943ddb292cc335f7b
umaji bagal <bagalumaji@gmail.com>
2025-01-02 08:07:05 +0530

    updated methods

--- driver/driver.py
+++ driver/driver.py
@@ -1,8 +1,16 @@
+from selenium import webdriver
+
+from driver.drivermanager import DriverManager
+
+
 class Driver:
     @staticmethod
     def init_driver():
-        pass
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        DriverManager.set_driver(driver)
 
     @staticmethod
     def quit_driver():
-        pass
+        DriverManager.get_driver().quit()
+        DriverManager.unload()
