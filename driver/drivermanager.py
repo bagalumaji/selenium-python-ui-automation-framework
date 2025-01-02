04e661ea70aa22a583dafe20bc86c23ec8c7632b
umaji bagal <bagalumaji@gmail.com>
2025-01-02 08:12:05 +0530

    added type hints to methods

--- driver/drivermanager.py
+++ driver/drivermanager.py
@@ -1,18 +1,19 @@
 import threading
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class DriverManager:
     _thread_local_driver = threading.local()
 
     @staticmethod
-    def get_driver():
+    def get_driver() -> WebDriver:
         return getattr(DriverManager._thread_local_driver, "driver", None)
 
     @staticmethod
-    def set_driver(driver):
+    def set_driver(driver: WebDriver) -> None:
         DriverManager._thread_local_driver.driver = driver
 
     @staticmethod
-    def unload():
+    def unload() -> None:
         if hasattr(DriverManager._thread_local_driver, "driver"):
             del DriverManager._thread_local_driver.driver
