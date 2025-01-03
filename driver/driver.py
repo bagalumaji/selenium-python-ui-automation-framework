dc0ca5c3780cc66fe234e6446aa64c4de20770a0
umaji bagal <bagalumaji@gmail.com>
2025-01-03 23:00:55 +0530

    added type hints to methods

--- driver/driver.py
+++ driver/driver.py
@@ -5,12 +5,12 @@ from driver.drivermanager import DriverManager
 
 class Driver:
     @staticmethod
-    def init_driver():
+    def init_driver() -> None:
         driver = webdriver.Chrome()
         driver.maximize_window()
         DriverManager.set_driver(driver)
 
     @staticmethod
-    def quit_driver():
+    def quit_driver() -> None:
         DriverManager.get_driver().quit()
         DriverManager.unload()
