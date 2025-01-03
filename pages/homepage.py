cf03c51f6ab361105f36f5f3198987b1544c70a2
umaji bagal <bagalumaji@gmail.com>
2025-01-03 23:40:38 +0530

    added my account locator and method

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,2 +1,10 @@
+from selenium.webdriver.common.by import By
+
+from driver.drivermanager import DriverManager
+
+
 class HomePage:
-    pass
+    _MY_ACCOUNT = By.LINK_TEXT, "My Account"
+
+    def click_on_my_account_link(self):
+        DriverManager.get_driver().find_element(*self._MY_ACCOUNT).click()
