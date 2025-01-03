14fb0093ceb5c941704049a5bde362eff13fda90
umaji bagal <bagalumaji@gmail.com>
2025-01-03 23:28:23 +0530

    updated driver factory

--- factory/driverfactory.py
+++ factory/driverfactory.py
@@ -1,4 +1,12 @@
+from selenium import webdriver
+
+
 class DriverFactory:
+    _driver_factory = {
+        "chrome": webdriver.Chrome(),
+        "firefox": webdriver.Firefox()
+    }
+
     @staticmethod
-    def get_driver():
-        pass
+    def get_driver(browser):
+        return DriverFactory._driver_factory.get(browser)
