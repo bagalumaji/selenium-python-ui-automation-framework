7b9a27e42671c02b0527a983842750e112f42a15
umaji bagal <bagalumaji@gmail.com>
2025-01-03 23:41:00 +0530

    created login test class and added test method

---
+++ tests/test_login.py
@@ -0,0 +1,13 @@
+import time
+
+import pytest
+
+from fixtures.conftest import setup_and_teardown
+from pages.homepage import HomePage
+
+
+@pytest.mark.usefixtures('setup_and_teardown')
+class TestLogin:
+    def test_verify_login_for_valid_credential(self):
+        home_page = HomePage()
+        home_page.click_on_my_account_link()
