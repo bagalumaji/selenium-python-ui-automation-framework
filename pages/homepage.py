88968f05f66ab817a2ce908cebbda85b8f85a6a7
umaji bagal <bagalumaji@gmail.com>
2025-01-03 23:43:42 +0530

    updated method

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
-from driver.drivermanager import DriverManager
+from pageaction.pageaction import PageAction
 
 
 class HomePage:
     _MY_ACCOUNT = By.LINK_TEXT, "My Account"
 
     def click_on_my_account_link(self):
-        DriverManager.get_driver().find_element(*self._MY_ACCOUNT).click()
+        PageAction.click(self._MY_ACCOUNT)
