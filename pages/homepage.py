576c9e938766fcec5f57f017550609f62f1f3382
umaji bagal <bagalumaji@gmail.com>
2025-01-04 09:24:45 +0530

    added click on login method

--- pages/homepage.py
+++ pages/homepage.py
@@ -5,6 +5,10 @@ from pageaction.pageaction import PageAction
 
 class HomePage:
     _MY_ACCOUNT = By.LINK_TEXT, "My Account"
+    _LOGIN = By.LINK_TEXT, "Login"
 
     def click_on_my_account_link(self):
         PageAction.click(self._MY_ACCOUNT, 10)
+
+    def click_on_login(self):
+        PageAction.click(self._LOGIN, 10)
