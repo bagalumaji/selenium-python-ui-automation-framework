9de380de4e99bf50e554605df39f3fd9d5f1bf91
umaji bagal <bagalumaji@gmail.com>
2025-01-04 08:57:04 +0530

    updated page action with explicit wait

--- pageaction/pageaction.py
+++ pageaction/pageaction.py
@@ -1,10 +1,10 @@
-from driver.drivermanager import DriverManager
+from wait.wait_factory import WaitFactory
 
 
 class PageAction:
     @staticmethod
-    def click(locator):
-        DriverManager.get_driver().find_element(*locator).click()
+    def click(locator, timeout):
+        WaitFactory.wait_for_element_to_be_clickable(locator, timeout).click()
 
     @staticmethod
     def type():
