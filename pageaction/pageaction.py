b6704a1f07ebe97f57f3c9f16f2d73554d142adc
umaji bagal <bagalumaji@gmail.com>
2025-01-04 10:30:02 +0530

    added type hints

--- pageaction/pageaction.py
+++ pageaction/pageaction.py
@@ -1,16 +1,18 @@
+from appium.webdriver import WebElement
+
 from wait.wait_factory import WaitFactory
 
 
 class PageAction:
     @staticmethod
-    def click(locator, timeout):
+    def click(locator: tuple or WebElement, timeout: int):
         WaitFactory.wait_for_element_to_be_clickable(locator, timeout).click()
 
     @staticmethod
-    def type(locator, timeout, message):
+    def type(locator: tuple or WebElement, timeout: int, message: str):
         print("locator", locator)
         WaitFactory.wait_for_element_to_be_clickable(locator, timeout).send_keys(message)
 
     @staticmethod
-    def is_displayed(locator, timeout):
+    def is_displayed(locator: tuple or WebElement, timeout: int):
         return WaitFactory.wait_for_element_to_be_visible(locator, timeout)
