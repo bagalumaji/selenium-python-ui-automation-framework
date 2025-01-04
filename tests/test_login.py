f420ce9d517e35c520d38b70e39ae85c149f4c2b
umaji bagal <bagalumaji@gmail.com>
2025-01-04 10:29:36 +0530

    updated test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,10 @@
+import time
+
 import pytest
 
 from fixtures.conftest import setup_and_teardown
 from pages.homepage import HomePage
+from pages.login.login_page import LoginPage
 
 
 @pytest.mark.usefixtures('setup_and_teardown')
@@ -10,3 +13,8 @@ class TestLogin:
         home_page = HomePage()
         home_page.click_on_my_account_link()
         home_page.click_on_login()
+        login_page = LoginPage()
+        login_page.enter_email("demo@gmail.com")
+        login_page.enter_password("test@123")
+        login_page.click_on_login_button()
+        time.sleep(5)
