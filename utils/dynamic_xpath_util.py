8ad524cbf4a1d291debbf749e93b0b829913dede
umaji bagal <bagalumaji@gmail.com>
2025-01-04 10:28:01 +0530

    updated create xpath method

--- utils/dynamic_xpath_util.py
+++ utils/dynamic_xpath_util.py
@@ -1,8 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-def create_xpath(xpath, text) -> By:
-    return By.XPATH(xpath.format(text))
-
-
-create_xpath("//label[normalize-space()='text']/following-sibling::input", )
+class DynamicXpathUtil:
+    @staticmethod
+    def create_xpath(xpath: str, text1: str) -> tuple:
+        return By.XPATH, xpath.format(text=text1)
