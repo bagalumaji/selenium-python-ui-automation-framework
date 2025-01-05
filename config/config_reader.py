29ec07448cd66a2edaa6798a59bd14f81055e030
umaji bagal <bagalumaji@gmail.com>
2025-01-05 09:33:19 +0530

    added code to read the config

--- config/config_reader.py
+++ config/config_reader.py
@@ -1,5 +1,27 @@
+import configparser
+
+from constants.framework_constants import FrameworkConstants
+
 
 class ConfigReader:
-    @staticmethod
-    def get_value(type, key_name: str):
-        pass
+    _instance = None
+    _config = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(ConfigReader, cls).__new__(cls, *args, **kwargs)
+            cls._config = configparser.RawConfigParser()
+            cls._config.read(FrameworkConstants.FRAMEWORK_CONFIG_FILE_NAME)
+        return cls._instance
+
+    def get_url(self) -> str:
+        return self._config.get("basic_info", "url")
+
+    def get_user_name(self) -> str:
+        return self._config.get("basic_info", "username")
+
+    def get_password(self) -> str:
+        return self._config.get("basic_info", "password")
+
+    def get_browser(self):
+        return self._config.get("basic_info", "browser")
