ed584871ce0bab3866c11cee37cdaa362d7b4666
umaji bagal <bagalumaji@gmail.com>
2025-01-06 08:46:16 +0530

    updated method with class method decorator

--- driver/drivermanager.py
+++ driver/drivermanager.py
@@ -1,19 +1,20 @@
 import threading
+
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class DriverManager:
     _thread_local_driver = threading.local()
 
-    @staticmethod
-    def get_driver() -> WebDriver:
-        return getattr(DriverManager._thread_local_driver, "driver", None)
+    @classmethod
+    def get_driver(cls) -> WebDriver:
+        return getattr(cls._thread_local_driver, "driver", None)
 
-    @staticmethod
-    def set_driver(driver: WebDriver) -> None:
-        DriverManager._thread_local_driver.driver = driver
+    @classmethod
+    def set_driver(cls, driver: WebDriver) -> None:
+        cls._thread_local_driver.driver = driver
 
-    @staticmethod
-    def unload() -> None:
-        if hasattr(DriverManager._thread_local_driver, "driver"):
-            del DriverManager._thread_local_driver.driver
+    @classmethod
+    def unload(cls) -> None:
+        if hasattr(cls._thread_local_driver, "driver"):
+            del cls._thread_local_driver.driver
