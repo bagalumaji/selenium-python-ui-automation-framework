5f2a47c477e72a0f4d099f06096eebd42db7c125
umaji bagal <bagalumaji@gmail.com>
2025-01-08 11:23:27 +0530

    updated with condition of driver creation

--- driver/driver.py
+++ driver/driver.py
@@ -6,12 +6,13 @@ from enums.browser import Browser
 
 class Driver:
     @staticmethod
-    def init_driver(browser_name:Browser) -> None:
-        config = ConfigReader()
-        driver = DriverFactory.get_driver(browser_name)
-        driver.maximize_window()
-        driver.get(config.url())
-        DriverManager.set_driver(driver)
+    def init_driver(browser_name: Browser) -> None:
+        if not DriverManager.get_driver():
+            config = ConfigReader()
+            driver = DriverFactory.get_driver(browser_name)
+            driver.maximize_window()
+            driver.get(config.url())
+            DriverManager.set_driver(driver)
 
     @staticmethod
     def quit_driver() -> None:
