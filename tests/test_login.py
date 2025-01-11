ad0048dc6218663c0216e16fa0ec801f7609d670
umaji bagal <bagalumaji@gmail.com>
2025-01-11 21:50:59 +0530

    updated test with username and password from config

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,18 +2,21 @@ import time
 
 import pytest
 
+from config.config_reader import ConfigReader
+from conftest import setup_and_teardown
 from pages.homepage import HomePage
 from pages.login.login_page import LoginPage
-from conftest import setup_and_teardown
+
 
 @pytest.mark.usefixtures('setup_and_teardown')
 class TestLogin:
     def test_verify_login_for_valid_credential(self):
+        c1 = ConfigReader()
         home_page = HomePage()
         home_page.click_on_my_account_link()
         home_page.click_on_login()
         login_page = LoginPage()
-        login_page.enter_email("demo@gmail.com")
-        login_page.enter_password("test@123")
+        login_page.enter_email(c1.user_name())
+        login_page.enter_password(c1.password())
         login_page.click_on_login_button()
         time.sleep(5)
