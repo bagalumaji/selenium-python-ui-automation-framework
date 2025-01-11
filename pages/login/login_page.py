1e91a510867f600dde2292d9a909f2514fb66b83
umaji bagal <bagalumaji@gmail.com>
2025-01-11 21:54:32 +0530

    added login to application method

--- pages/login/login_page.py
+++ pages/login/login_page.py
@@ -11,16 +11,21 @@ class LoginPage:
     _LOGIN_BUTTON = By.XPATH, "//input[@value='Login']"
     _config = ConfigReader()
 
-    def enter_email(self, user_name):
+    def _enter_email(self, user_name):
         email_locator = DynamicXpathUtil.create_xpath(self._XPATH_FOR_EMAIL_AND_PASSWORD,
                                                       LoginPageConstants.EMAIL_ID_ADDRESS)
         print(email_locator)
         PageAction.type(email_locator, self._config.default_timeout(), user_name)
 
-    def enter_password(self, password):
+    def _enter_password(self, password):
         password_locator = DynamicXpathUtil.create_xpath(self._XPATH_FOR_EMAIL_AND_PASSWORD,
                                                          LoginPageConstants.PASSWORD_TEXT)
         PageAction.type(password_locator, self._config.default_timeout(), password)
 
-    def click_on_login_button(self):
+    def _click_on_login_button(self):
         PageAction.click(self._LOGIN_BUTTON, self._config.default_timeout())
+
+    def login_to_application(self, username: str, password: str):
+        self._enter_email(username)
+        self._enter_password(password)
+        self._click_on_login_button()
