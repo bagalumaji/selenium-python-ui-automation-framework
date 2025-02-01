46c18281f4609d6b96ddc793081217a16ea60fa8
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-02-01 10:16:38 +0530

    added exception handling

--- driver/drivermanager.py
+++ driver/drivermanager.py
@@ -1,20 +1,55 @@
 import threading
 
+import threading
 from selenium.webdriver.remote.webdriver import WebDriver
+import logging
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class DriverManager:
     _thread_local_driver = threading.local()
 
     @classmethod
     def get_driver(cls) -> WebDriver:
+        """
+        Retrieve the WebDriver instance for the current thread.
+        :return: WebDriver instance or None if not set.
+        """
         return getattr(cls._thread_local_driver, "driver", None)
 
     @classmethod
     def set_driver(cls, driver: WebDriver) -> None:
+        """
+        Set the WebDriver instance for the current thread.
+        :param driver: WebDriver instance to be set.
+        """
         cls._thread_local_driver.driver = driver
+        logger.info("Driver set for the current thread.")
 
     @classmethod
     def unload(cls) -> None:
+        """
+        Unload the WebDriver instance for the current thread.
+        """
         if hasattr(cls._thread_local_driver, "driver"):
             del cls._thread_local_driver.driver
+            logger.info("Driver unloaded for the current thread.")
+
+
+# class DriverManager:
+#     _thread_local_driver = threading.local()
+#
+#     @classmethod
+#     def get_driver(cls) -> WebDriver:
+#         return getattr(cls._thread_local_driver, "driver", None)
+#
+#     @classmethod
+#     def set_driver(cls, driver: WebDriver) -> None:
+#         cls._thread_local_driver.driver = driver
+#
+#     @classmethod
+#     def unload(cls) -> None:
+#         if hasattr(cls._thread_local_driver, "driver"):
+#             del cls._thread_local_driver.driver
