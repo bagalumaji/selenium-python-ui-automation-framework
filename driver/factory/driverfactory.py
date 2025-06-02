316852806078815722d932ac482ded6968eaee31
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 13:35:46 +0530

    removed comments

--- driver/factory/driverfactory.py
+++ driver/factory/driverfactory.py
@@ -9,9 +9,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class DriverFactory:
-    """
-    Factory class to create WebDriver instances based on the browser type.
-    """
     _driver_factory = {
         Browser.CHROME: lambda: webdriver.Chrome(),
         Browser.FIREFOX: lambda: webdriver.Firefox()
@@ -19,11 +16,6 @@ class DriverFactory:
 
     @classmethod
     def get_driver(cls, browser: Browser):
-        """
-        Retrieve a WebDriver instance for the specified browser.
-        :param browser: Browser enum value indicating the browser type.
-        :return: WebDriver instance.
-        """
         try:
             driver = cls._driver_factory.get(browser)()
             logger.info(f"{browser.name} driver created successfully.")
@@ -33,12 +25,3 @@ class DriverFactory:
             raise ValueError(f"No driver found for browser: {browser.name}")
 
 
-# class DriverFactory:
-#     _driver_factory = {
-#         Browser.CHROME: lambda: webdriver.Chrome(),
-#         Browser.FIREFOX: lambda: webdriver.Firefox()
-#     }
-#
-#     @classmethod
-#     def get_driver(cls, browser: Browser):
-#         return cls._driver_factory.get(browser)()
